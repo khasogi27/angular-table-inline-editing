84a8aaf0a8c325d353672cdffec3fb1a073c703c
khasogi27 <55776916+khasogi27@users.noreply.github.com>
2023-03-30 09:57:53 +0700

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { dsGridUser, dsTreeGridUser } from './t-grid/local-data';
 // import { BackgroundType, FieldType } from './t-grid/grid/grid.component';
 import {
   BackgroundType,
   FieldType,
 } from './t-grid/tree-grid/tree-grid.component';
-
-// {
-//   userId: '56748fb3e3',
-//   name: 'Paweł',
-//   title: '1',
-//   email: 'paweluna@howstuffworks.com',
-//   contract: true,
-//   date: '20-10-1997',
-//   domain: '.comodo',
-//   country: 'mamarika',
-// },
-// {
-//   userId: '6de39701b',
-//   name: 'Jeffie',
-//   title: '2',
-//   email: 'jlewzey1@seesaa.net',
-//   contract: false,
-//   date: '20-10-1997',
-//   domain: '.idomaret',
-//   country: 'indonesia',
-// },
-// {
-//   userId: '8b3a6dd017',
-//   name: 'Mallory',
-//   title: '3',
-//   email: 'mhulme2@domainmarket.com',
-//   contract: true,
-//   date: '20-10-1997',
-//   domain: '.net',
-//   country: 'italy',
-// },
 
 interface User {
   id: number | string;
@@ -52,6 +22,7 @@ interface User {
 })
 export class AppComponent implements OnInit {
   @ViewChild('grid') grid: any;
+  @ViewChild('treegrid') treegrid: any;
 
   public gridField: FieldType[] | any = [
     // {
@@ -113,6 +84,9 @@ export class AppComponent implements OnInit {
       text: 'admin case',
     },
   ];
+  public dsKey: string = 'userId';
+  public dsUser: User[] | any;
+  public gridType: BackgroundType = 'normal';
 
   public treeGridField: FieldType[] | any = [
     // {
@@ -174,111 +148,19 @@ export class AppComponent implements OnInit {
     //   text: 'admin case',
     // },
   ];
-
   public dsTreeKey: string = 'userId';
-  public dsKey: string = 'userId';
-
-  public dsTempUser: User[] | any = [];
-
-  public dsUser: User[] | any = [
-    {
-      userId: '56748fb3e3',
-      name: 'Paweł',
-      title: '1',
-      email: 'paweluna@howstuffworks.com',
-      contract: true,
-      date: '20-10-1997',
-      domain: '.comodo',
-      country: 'mamarika',
-    },
-    {
-      userId: '6de39701b',
-      name: 'Jeffie',
-      title: '2',
-      email: 'jlewzey1@seesaa.net',
-      contract: false,
-      date: '21-11-1998',
-      domain: '.idomaret',
-      country: 'indonesia',
-    },
-    {
-      userId: '8b3a6dd017',
-      name: 'Mallory',
-      title: '3',
-      email: 'mhulme2@domainmarket.com',
-      contract: true,
-      date: '22-12-1999',
-      domain: '.net',
-      country: 'italy',
-    },
-  ];
-
-  public dsTreeUser: User[] | any = [
-    {
-      userId: '56748fb3e3',
-      name: 'Paweł',
-      title: '1',
-      contract: true,
-      email: 'paweluna@howstuffworks.com',
-      children: [
-        {
-          domain: '.comodo',
-          children: [
-            {
-              date: '20-10-1997',
-              country: 'mamarika',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      userId: '6de39701b',
-      name: 'Jeffie',
-      title: '2',
-      contract: false,
-      email: 'jlewzey1@seesaa.net',
-      children: [
-        {
-          domain: '.idomaret',
-          children: [
-            {
-              date: '21-11-1998',
-              country: 'indonesia',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      userId: '8b3a6dd017',
-      name: 'Mallory',
-      title: '3',
-      contract: true,
-      email: 'mhulme2@domainmarket.com',
-      children: [
-        {
-          domain: '.net',
-          children: [
-            {
-              date: '22-12-1999',
-              country: 'italy',
-            },
-          ],
-        },
-      ],
-    },
-  ];
-
-  public gridType: BackgroundType = 'normal';
+  public dsTreeUser: User[] | any;
   public treeGridType: BackgroundType = 'flat';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dsUser = dsGridUser;
+    this.dsTreeUser = dsTreeGridUser;
+  }
 
   onSaveClick() {
-    console.log(this.grid.tableValue, '<<<');
+    console.log(this.grid.tableValue, '<<< grid');
     // this.grid.newData$.subscribe((e) => {
     //   console.log(e, '<<< event');
     //   // this.grid.newData$.complete();
---
+++ src/app/t-grid/local-data.ts
@@ -0,0 +1,89 @@
+export const dsGridUser = [
+  {
+    userId: '56748fb3e3',
+    name: 'Paweł',
+    title: '1',
+    email: 'paweluna@howstuffworks.com',
+    contract: true,
+    date: '20-10-1997',
+    domain: '.comodo',
+    country: 'mamarika',
+  },
+  {
+    userId: '6de39701b',
+    name: 'Jeffie',
+    title: '2',
+    email: 'jlewzey1@seesaa.net',
+    contract: false,
+    date: '20-10-1997',
+    domain: '.idomaret',
+    country: 'indonesia',
+  },
+  {
+    userId: '8b3a6dd017',
+    name: 'Mallory',
+    title: '3',
+    email: 'mhulme2@domainmarket.com',
+    contract: true,
+    date: '20-10-1997',
+    domain: '.net',
+    country: 'italy',
+  },
+];
+
+export const dsTreeGridUser = [
+    {
+      userId: '56748fb3e3',
+      name: 'Paweł',
+      title: '1',
+      contract: true,
+      email: 'paweluna@howstuffworks.com',
+      children: [
+        {
+          domain: '.comodo',
+          children: [
+            {
+              date: '20-10-1997',
+              country: 'mamarika',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      userId: '6de39701b',
+      name: 'Jeffie',
+      title: '2',
+      contract: false,
+      email: 'jlewzey1@seesaa.net',
+      children: [
+        {
+          domain: '.idomaret',
+          children: [
+            {
+              date: '21-11-1998',
+              country: 'indonesia',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      userId: '8b3a6dd017',
+      name: 'Mallory',
+      title: '3',
+      contract: true,
+      email: 'mhulme2@domainmarket.com',
+      children: [
+        {
+          domain: '.net',
+          children: [
+            {
+              date: '22-12-1999',
+              country: 'italy',
+            },
+          ],
+        },
+      ],
+    },
+  ];
